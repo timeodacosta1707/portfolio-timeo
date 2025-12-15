8c7f26ffefac64b16baeb0cfe320e7a8e1c18a2f
dacostatimeo <dacostatimeo@gmail.com>
2025-12-16 00:30:18 +0100

    landing screen finished

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Accueil } from './components/accueil/accueil';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: 'app-root',
+	imports: [
+		RouterOutlet,
+		Accueil,
+		Navbar
+	],
+	templateUrl: './app.html',
+	styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('portfolio');
+	protected readonly title = signal('portfolio');
 }
---
+++ src/app/technologies/technologies.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technologies',
+  imports: [],
+  templateUrl: './technologies.html',
+  styleUrl: './technologies.scss',
+})
+export class Technologies {
+
+}
