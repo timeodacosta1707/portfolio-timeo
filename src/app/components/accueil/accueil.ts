4841f68104c384852c3ac3b568c5d3edb4102ea0
dacostatimeo <dacostatimeo@gmail.com>
2025-12-16 01:49:07 +0100

    update accueil, age update when it's my bday

--- src/app/components/accueil/accueil.ts
+++ src/app/components/accueil/accueil.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-accueil',
@@ -6,12 +6,32 @@ import { Component } from '@angular/core';
 	templateUrl: './accueil.html',
 	styleUrl: './accueil.scss',
 })
-export class Accueil {
+export class Accueil implements OnInit {
+	age!: number;
+
+	ngOnInit(): void {
+		this.calculateAge();
+	}
+
 	scrollToTechnologies() {
 		const element = document.getElementById('technologies');
 
 		if (element) {
 			element.scrollIntoView({ behavior: 'smooth', block: 'start' });
 		}
+	}
+
+	calculateAge() {
+		const birthDate = new Date('2006-07-17');
+		const today = new Date();
+
+		let age = today.getFullYear() - birthDate.getFullYear();
+		const monthDifference = today.getMonth() - birthDate.getMonth();
+
+		if(monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+			age--;
+		}
+
+		this.age = age;
 	}
 }
